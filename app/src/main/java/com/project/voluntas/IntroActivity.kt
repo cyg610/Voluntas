897afc329b0fa13387ce467eb99643ecc1a13b98
cyg610 <cyg610@github.com>
2021-05-01 18:39:34 +0900

    0430 - 인트로 화면 구성

---
+++ app/src/main/java/com/project/voluntas/IntroActivity.kt
@@ -0,0 +1,27 @@
+package com.project.voluntas
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        var handler = Handler()
+        handler.postDelayed({
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        },3000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+}
