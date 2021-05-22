242a93e418142562b3780370b7a2f3f03ccccc62
cyg610 <cyg61072@gmail.com>
2021-05-22 14:44:17 +0900

    05.22 - 메인 화면 구현 완료

--- app/src/main/java/com/project/voluntas/MainActivity.kt
+++ app/src/main/java/com/project/voluntas/MainActivity.kt
@@ -2,12 +2,17 @@ package com.project.voluntas
 
 import android.os.Build
 import android.os.Bundle
+import android.os.StrictMode
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.project.voluntas.databinding.ActivityMainBinding
+import org.w3c.dom.Document
 import org.w3c.dom.Element
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
 import java.net.URL
 import javax.xml.parsers.DocumentBuilderFactory
 
@@ -23,16 +28,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        if(android.os.Build.VERSION.SDK_INT >9){
+            val policy : StrictMode.ThreadPolicy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+        }
+
         RecyclerView = findViewById<RecyclerView>(R.id.RecyclerView)
 
-        //네트워크 스레드
+        var sido = intent.getStringExtra("SidoKey")
+        val key: String ="1O5TyVjRbo1%2FC5JVf9%2FNZIV2D6FSMXBUZe0MVRTwYQBFnk2GFESxQSZ1zLoJkddQWKRSjJ0y78xRxZt0Zo0S2g%3D%3D"
+        val url: String="http://apis.data.go.kr/1383000/YouthActivInfoVolSrvc/getVolProgrmList?pageNo=1&numOfRows=10&sido="+sido+"&sdate=20210105&edate=20210501&type=xml&serviceKey="+key
+
+        val xml : Document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(url)
+        xml.documentElement.normalize()
+
+        val list:NodeList = xml.getElementsByTagName("item")
+
+        for(i in 0..list.length-1){
+
+            var n: Node = list.item(i)
+            if(n.nodeType==Node.ELEMENT_NODE){
+                val elem = n as Element
+                val map = mutableMapOf<String, String>()
+                for(j in 0..elem.attributes.length-1){
+                    map.putIfAbsent(elem.attributes.item(j).nodeName, elem.attributes.item(j).nodeValue)
+                }
 
 
-        var thread = NetworkThread()
-        thread.start()
+                initData("${elem.getElementsByTagName("organNm").item(0).textContent}","${elem.getElementsByTagName("pgmNm").item(0).textContent}","${elem.getElementsByTagName("sdate").item(0).textContent}",1)
 
-        data1.add(Data("sdfsdf","","",1))
+            }
 
+        }
 
         binding.RecyclerView.adapter = adapter
         binding.RecyclerView.layoutManager = LinearLayoutManager(this)
@@ -40,51 +67,15 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    inner class NetworkThread :Thread() {
-        @RequiresApi(Build.VERSION_CODES.Q)
-        override fun run() {
-            try {
-
-
-                //URL DATA
-                var sido = intent.getStringExtra("SidoKey")
-                val key: String ="1O5TyVjRbo1%2FC5JVf9%2FNZIV2D6FSMXBUZe0MVRTwYQBFnk2GFESxQSZ1zLoJkddQWKRSjJ0y78xRxZt0Zo0S2g%3D%3D"
-                val url: String="http://apis.data.go.kr/1383000/YouthActivInfoVolSrvc/getVolProgrmList?pageNo=1&numOfRows=10&sido="+sido+"&sdate=20210105&edate=20210501&type=xml&serviceKey="+key
-
-                var data = URL(url)
-                var conn = data.openConnection()
-                var input = conn.getInputStream()
-                var factory = DocumentBuilderFactory.newInstance()
-                var builder = factory.newDocumentBuilder()
-                var doc = builder.parse(input)
-                var root = doc.documentElement
-                var item_node_list = root.getElementsByTagName("item")
-
-                for (i in 0 until item_node_list.length){
-                    var item_element = item_node_list.item(i) as Element
-                    var organNM_list = item_element.getElementsByTagName("organNm")
-
-                    var organNM_node = organNM_list.item(0) as Element
-                    var organNM = organNM_node.textContent
-
-                    runOnUiThread{
-                     //   Log.d("APITEST","봉사기관: ${organNM}\n")
-
-                        var Add_Data = Data("${organNM}","가나다라","",1)
-                        data1.add(Add_Data)
-
-                    }
-
-                }
-            }catch (e: Exception){
-                e.printStackTrace()
-            }
-        }
-
+    private fun initData(title: String, work: String, date: String, key: Int) {
+        adapter.additem(title, work, date, key)
     }
 
 
 }
+
+
+
 
 
 
--- app/src/main/java/com/project/voluntas/MainListAdapter.kt
+++ app/src/main/java/com/project/voluntas/MainListAdapter.kt
@@ -1,15 +1,18 @@
 package com.project.voluntas
 
+import android.provider.ContactsContract
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.project.voluntas.databinding.ListviewItemBinding
+import java.sql.DataTruncation
+import com.project.voluntas.Data as VoluntasData
 
 
-class MainListAdapter(data1: ArrayList<Data>) :RecyclerView.Adapter<Holder>() {
+class MainListAdapter(data1: ArrayList<com.project.voluntas.Data>) :RecyclerView.Adapter<Holder>() {
 
-        var listData = data1
+        val listData = data1
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
             val binding = ListviewItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
             return Holder(binding)
@@ -24,13 +27,26 @@ class MainListAdapter(data1: ArrayList<Data>) :RecyclerView.Adapter<Holder>() {
             holder.setRecycler(data)
         }
 
+    fun additem(title: String, work: String, date: String, key: Int) {
+
+        val item = com.project.voluntas.Data(title, work, date, key)
+        item.settitle(title)
+        item.setdate(date)
+        item.setwork(work)
+        item.setkey(key)
+        listData.add(item)
 
 
     }
 
+
+}
+
     class Holder(val binding: ListviewItemBinding) : RecyclerView.ViewHolder(binding.root){
-        fun setRecycler(data: Data){
+        fun setRecycler(data: VoluntasData){
             binding.ListViewItemTitle.text = "${data.gettitle()}"
+            binding.ListViewItemWork.text = "${data.getwork()}"
+            binding.ListViewItemDate.text = "${data.getdate()}"
         }
     }
 
